db821c6383d376538abe8b32ad1c8c8110158753
gong <517441540@qq.com>
2023-07-05 07:52:23 +0800

    fix: bugfix

--- src/pages/Editor/canvas/components/RenderMesh.tsx
+++ src/pages/Editor/canvas/components/RenderMesh.tsx
@@ -1,8 +1,8 @@
 import type { FC } from 'react'
 import { memo, useRef, useState } from 'react'
-import { useFrame, useThree } from '@react-three/fiber'
+import { useFrame } from '@react-three/fiber'
 import isEqual from 'react-fast-compare'
-import { useDrag } from 'react-use-gesture'
+import { OrbitControls, TransformControls } from '@react-three/drei'
 
 import store from '@/store'
 import type { MeshType } from '@/type/SchemaType'
@@ -13,22 +13,13 @@ interface RenderMeshProps {
 
 const RenderMesh: FC<RenderMeshProps> = ({ mesh }) => {
   const meshRef = useRef(null)
+  const transform = useRef(null)
+  const orbit = useRef(null)
   const { position, geometry, material } = mesh
   const [currentPosition, setCurrentPosition] = useState([position.x, position.y, position.z])
   const [hovered, setHover] = useState(false)
   const schemaStore = store.schemaStore(state => state)
   const { setOrbitControlsEnabled } = store.settingStore(state => state)
-  const { size, viewport } = useThree()
-  const aspect = size.width / viewport.width
-
-  const bind = useDrag(
-    ({ offset: [x, y] }) => {
-      setOrbitControlsEnabled(false)
-      const z = currentPosition[2]
-      setCurrentPosition([x / aspect, -y / aspect, z])
-    },
-    { pointerEvents: true },
-  )
 
   useFrame(() => {
 
@@ -38,32 +29,23 @@ const RenderMesh: FC<RenderMeshProps> = ({ mesh }) => {
     schemaStore.setCurrentSelectedMesh(mesh)
   }
 
-  // const updateMesh = () => {
-  //   schemaStore.updateMesh({
-  //     ...mesh,
-  //     position: {
-  //       x: currentPosition[0],
-  //       y: currentPosition[1],
-  //       z: currentPosition[2],
-  //     },
-  //   })
-  // }
-
   return (
-    <mesh
-      ref={meshRef}
-      position={currentPosition as [number, number, number]}
-      {...bind() as any}
-      scale={1}
-      onClick={setCurrentMesh}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-      onPointerLeave={() => setOrbitControlsEnabled(true)}
+    <>
+      <TransformControls ref={transform}>
+        <mesh
+          ref={meshRef}
+          position={currentPosition as [number, number, number]}
+          scale={1}
+          onClick={setCurrentMesh}
+          onPointerOver={() => setHover(true)}
+          onPointerOut={() => setHover(false)}
       >
+          <boxGeometry args={[geometry.width, geometry.height, geometry.depth]} />
+          <meshStandardMaterial color={hovered ? 'hotpink' : material.color || 'red'} />
+        </mesh>
+      </TransformControls>
+    </>
 
-      <boxGeometry args={[geometry.width, geometry.height, geometry.depth]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : material.color || 'red'} />
-    </mesh>
   )
 }
 
