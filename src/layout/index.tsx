54b8cadd99667d14057174904240fd08bd24f6b4
gongzhen <517441540@qq.com>
2023-07-17 10:14:36 +0800

    fix: add lodding

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import { Outlet } from '@umijs/max'
 import { ConfigProvider } from 'antd'
+import ReactLoading from '@/components/Loding'
 import { ErrorBoundary } from '@/components/ErrorBoundary'
 import { ErrorFallback } from '@/components/ErrorFallback'
 
@@ -14,21 +15,24 @@ const EditorLayout = () => {
     <ErrorBoundary
       fallbackRender={ErrorFallback}
       >
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: '#00b96b',
-          },
-          components: {
-            Collapse: {
-              colorBorder: '#444',
+      <Suspense fallback={<ReactLoading />}>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: '#00b96b',
             },
-          },
-        }}>
-        <div style={{ width: '100vw', height: '100vh' }}>
-          { !hasError ? <Outlet/> : null}
-        </div>
-      </ConfigProvider>
+            components: {
+              Collapse: {
+                colorBorder: '#444',
+              },
+            },
+          }}>
+          <div style={{ width: '100vw', height: '100vh' }}>
+            { !hasError ? <Outlet/> : null}
+          </div>
+        </ConfigProvider>
+      </Suspense>
+
     </ErrorBoundary>
   )
 }
